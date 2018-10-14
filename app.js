e3001ca6211f312c78a43d7d49adea2e53c67626
evanr35 <erisas@umass.edu>
2018-10-13 23:26:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+var express = require('express');
+var http = require('http');
+
+// Setting the port number
+var port = process.env.PORT || 8000;
+
+// Initializing express
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+var server = http.createServer(app).listen(port);
